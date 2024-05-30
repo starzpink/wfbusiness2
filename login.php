e907ac1ea327f6f83c961c63d43585800d11e284
starzpink <anapaulinm@outlook.com>
2024-05-30 14:49:32 -0300

    adicão login

--- login.php
+++ login.php
@@ -1,8 +1,40 @@
-<?php
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/login.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form action="area_adm.php" method="POST">
+                        <p>Acesso à área do administrador</p>
+                        <?php if (isset($_SESSION['msg'])) { ?>
+                            <p style="color: red;"><?php echo $_SESSION['msg']; ?></p>
+                            <?php session_destroy(); ?>
+                        <?php } ?>
+                        <div class="form-group">
+                            <input type="text" name="email" placeholder="E-mail" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="senha" placeholder="Senha" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Entrar">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+        <!-- Scripts Bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </body>
+</html>
