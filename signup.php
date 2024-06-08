01835cf35b035621102f6080bfd9ff067c9d6f70
rene <rene.melo@fatec.sp.gov.br>
2024-06-08 13:08:17 -0300

    teste commit

--- signup.php
+++ signup.php
@@ -4,49 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Cadastro</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="css/signup.css">
     <script defer src="script.js"></script>
 </head>
 <body>
+    <nav class="navbar">
+        <a href="index.php">
+        <span class="logo">Workfolio for Business</span>
+        </a>
+    </nav>
     <div class="container">
-        <h2>Formulário de Cadastro</h2>
+        <h1>Cadastre-se com e-mail</h1>
+        <div class="formContainer">
         <form id="cadastroForm" action="process.php" method="post">
-            <label for="nome_emp">Nome da Empresa:</label>
-            <input type="text" id="nome_emp" name="nome_emp" required></br>
-
-            <label for="cod_local">Local:</label>
-            <input type="text" id="cod_local" name="cod_local" required></br>
-
-            <label for="areaat_emp">Área de Atuação:</label>
-            <input type="text" id="areaat_emp" name="areaat_emp" required></br>
-
-            <label for="desc_emp">Descrição:</label>
-            <textarea id="desc_emp" name="desc_emp"></textarea></br>
-
-            <label for="email_emp">E-mail de contato:</label>
-            <input type="email" id="email_emp" name="email_emp" required></br>
-
-            <label for="site_emp">Site:</label>
-            <input type="text" id="site_emp" name="site_emp" required></br>
-
+            <div class="campo">
+            <input type="text" id="nome_emp" name="nome_emp" placeholder="Nome da Empresa" required></br>
+            </div>
+            <div class="campo">
+            <input type="text" id="cod_local" name="cod_local" placeholder="Local" required></br>
+            </div>
+            <div class="campo">
+            <input type="text" id="areaat_emp" name="areaat_emp" placeholder="Área de Atuação" required></br>
+            </div>
+            <div class="campo">
+            <textarea id="desc_emp" name="desc_emp" placeholder="Descrição"></textarea></br>
+            </div>
+            <div class="campo">
+            <input type="email" id="email_emp" name="email_emp" placeholder="E-mail de contato" required></br>
+            </div>
+            <div class="campo">
+            <input type="text" id="site_emp" name="site_emp" placeholder="Site" required></br>
+            </div>
+            <div class="campo">
             <label for="tel_emp">Telefone:</label>
             <input type="text" id="tel_emp" name="tel_emp" required></br>
-
+            </div>
+            <div class="campo">
             <label for="cnpj_emp">CNPJ</label>
             <input type="text" id="cnpj_emp" name="cnpj_emp" required></br>
-
-<!--segunda parte-->
+            </div>
+            <!--segunda parte-->
+            <div class="campo2">
             <label for="email">E-mail de Login:</label>
             <input type="email" id="email" name="email" required></br>
-
+            </div>
+            <div class="campo2">
             <label for="senha">Senha:</label>
             <input type="password" id="confirmaSenha" name="confirmaSenha" required></br>
-
+            </div>
+            <div class="campo2">
             <label for="confirmaSenha">Confirme a Senha:</label>
             <input type="password" id="confirmaSenha" name="confirmaSenha" required></br>
-
+            </div>
             <button type="submit">Cadastrar</button>
         </form>
+        </div>
     </div>
 </body>
 </html>
