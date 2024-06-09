ea6a913318f2e30dd31a569c71577d2a5dba67a8
starzpink <anapaulinm@outlook.com>
2024-06-09 19:50:19 -0300

    insert emp ?

--- insert/insertEmpresa.php
+++ insert/insertEmpresa.php
@@ -1,26 +1,31 @@
 <?php
 session_start();
-$cod_usuario = $_SESSION['cod_usuario'];
 
 include '../conn.php';
 
-$conn->query("SET @cod_usuario = $cod_usuario");
-
 header('Content-type: application/json');
 
-$sql = "insert into empresa(nome_emp, cod_local, areaat_emp, desc_emp, email_emp, site_emp, tel_emp, cnpj_emp, cod_usuario) "
-        . "values ('".$_POST['nome_emp']."',"
-        .$_POST['cod_local'].",".$_POST['areaat_emp'].",'".$_POST['desc_emp']."','".$_POST['email_emp']."','"
-        .$_POST['site_emp']."','".$_POST['tel_emp']."','".$_POST['cnpj_emp']."',".$_SESSION['cod_usuario'].")";
+$sql_usuario = "INSERT INTO usuario (email, senha, cargo) "
+        . "VALUES ('".$_POST['email']."', MD5('".$_POST['senha']."'), 3)"; // Defina o cargo como desejado, no caso 3
+if($conn->query($sql_usuario) === TRUE){
+    $cod_usuario = $conn->insert_id; // Recupera o ID gerado pelo banco de dados
 
-if($conn->query($sql) === TRUE){
-    $msg = "Empresa adicionada com sucesso!";
+    // Insere a empresa na tabela 'empresa'
+    $sql_empresa = "INSERT INTO empresa(nome_emp, cod_local, areaat_emp, desc_emp, email_emp, site_emp, tel_emp, cnpj_emp, cod_usuario) "
+        . "VALUES ('".$_POST['nome_emp']."',"
+        .$_POST['cod_local'].",".$_POST['areaat_emp'].",'".$_POST['desc_emp']."','".$_POST['email_emp']."','"
+        .$_POST['site_emp']."','".$_POST['tel_emp']."','".$_POST['cnpj_emp']."',$cod_usuario)";
+
+    if($conn->query($sql_empresa) === TRUE){
+        $msg = "Empresa adicionada com sucesso!";
+    } else {
+        $msg = "Erro ao adicionar empresa: ".$conn->error;
+    }
 } else {
-    $msg = "Error: ".$sql."<br>".$conn->error;
+    $msg = "Erro ao adicionar usuário: ".$conn->error;
 }
 
 $conn->close();
 
 echo json_encode(['msg'=>$msg]);
-        
 ?>
