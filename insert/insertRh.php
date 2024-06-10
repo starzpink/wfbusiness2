f19018573f9ee1c63896b0fa28533c583c75b2ea
starzpink <anapaulinm@outlook.com>
2024-06-10 16:25:09 -0300

    insert rh consertado ?

--- insert/insertRh.php
+++ insert/insertRh.php
@@ -9,13 +9,13 @@ header('Content-type: application/json');
 $sql_usuario = "INSERT INTO usuario (email, senha, cargo) "
         . "VALUES ('".$_POST['email']."', MD5('".$_POST['senha']."'), 1)";
 if($conn->query($sql_usuario) === TRUE){
-    $cod_usuario = $conn->insert_id;
+    $cod_usuario = $conn->insert_id; // Recupera o ID gerado pelo banco de dados
     
     $conn->query("SET @cod_usuario = $cod_usuario");
 
-    $sql_rh = "INSERT INTO rh($cod_emp, nome_rh, cpf_rh, email_rh, tel_rh, cod_usuario) "
-        . "values ($cod_emp,'".$_POST['nome_rh']."',"
-        . $_POST['cpf_rh'].",'".$_POST['email_rh']."','".$_POST['tel_rh']."', $cod_usuario)";
+    $sql_rh = "INSERT INTO rh(cod_emp, nome_rh, cpf_rh, email_rh, tel_rh, cod_usuario) "
+        . "VALUES ($cod_emp,'".$_POST['nome_rh']."',"
+        .$_POST['cpf_rh'].",'".$_POST['email_rh']."','".$_POST['tel_rh']."', $cod_usuario)";
 
     if($conn->query($sql_rh) === TRUE){
         $msg = "RH adicionado com sucesso!";
