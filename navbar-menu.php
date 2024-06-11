6c9557954cf9b4f9aa6defcd9d977d38f87b13f4
rene <rene.melo@fatec.sp.gov.br>
2024-06-11 18:24:43 -0300

    navbar reclaimed!! minha cultura não é sua fantasia @bootstrap

--- navbar-menu.php
+++ navbar-menu.php
@@ -1,7 +1,7 @@
-<nav class="navbar">
+<nav class="navbarMINHA">
             <span class="logo">Workfolio for Business</span>
             <ul class="nav-menu">
-                <li><a href="login.php">Login</a></li>
-                <li><a href="signup.php">Cadastre-se</a></li>
+                <li><a href="login.php" style="color: white">Login</a></li>
+                <li><a href="signup.php" style="color: white">Cadastre-se</a></li>
             </ul>
 </nav>
