e2beb72f47e8af6fed56db4b8b1d6ab0a8ef9ce3
rene <rene.melo@fatec.sp.gov.br>
2024-06-16 18:42:39 -0300

    eagr

--- perfilrh.php
+++ perfilrh.php
@@ -61,30 +61,32 @@ $conn->close();
         <?php include 'sidebar.php'; ?>
     </nav>
     <div class="v-principal">
-        <h1>Perfil do RH</h1>
-        <div class="campos-container">
-            <div class="campo">
-                <label>Email do Usuário:</label>
-                <p><?php echo htmlspecialchars($rh['email']); ?></p>
-            </div>
-            <div class="campo">
-                <label>Nome:</label>
-                <p><?php echo htmlspecialchars($rh['nome_rh']); ?></p>
-            </div>
-            <div class="campo">
-                <label>CPF:</label>
-                <p><?php echo htmlspecialchars($rh['cpf_rh']); ?></p>
-            </div>
-            <div class="campo">
-                <label>E-mail:</label>
-                <p><?php echo htmlspecialchars($rh['email_rh']); ?></p>
-            </div>
-            <div class="campo">
-                <label>Telefone:</label>
-                <p><?php echo htmlspecialchars($rh['tel_rh']); ?></p>
-            </div>
-            <div class="dash-botoes">
-                <a href="editarrh.php?cod_usuario=<?php echo $cod_usuario; ?>" class="btn btn-primary">Editar</a>
+        <div class="container-perfil">
+            <h1>Perfil do RH</h1>
+            <div class="campos-container">
+                <div class="campo">
+                    <label>Email do Usuário:</label>
+                    <p><?php echo htmlspecialchars($rh['email']); ?></p>
+                </div>
+                <div class="campo">
+                    <label>Nome:</label>
+                    <p><?php echo htmlspecialchars($rh['nome_rh']); ?></p>
+                </div>
+                <div class="campo">
+                    <label>CPF:</label>
+                    <p><?php echo htmlspecialchars($rh['cpf_rh']); ?></p>
+                </div>
+                <div class="campo">
+                    <label>E-mail:</label>
+                    <p><?php echo htmlspecialchars($rh['email_rh']); ?></p>
+                </div>
+                <div class="campo">
+                    <label>Telefone:</label>
+                    <p><?php echo htmlspecialchars($rh['tel_rh']); ?></p>
+                </div>
+                <div class="dash-botoes">
+                    <a href="editarrh.php?cod_usuario=<?php echo $cod_usuario; ?>" class="btn btn-primary">Editar</a>
+                </div>
             </div>
         </div>
     </div>
